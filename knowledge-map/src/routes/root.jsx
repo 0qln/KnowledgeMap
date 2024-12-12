b701ccd6d91eddc43eb1986f12127eb27b4ba36d
Linus Nagel <linusnag@gmail.com>
2024-12-12 11:28:45 +0100

    add example graph with example data

--- knowledge-map/src/routes/root.jsx
+++ knowledge-map/src/routes/root.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Graph from "../components/graph";
 
 export default function Root() {
     return (
         <>
             <h1>Knowledge Map</h1>
+            <div id="graph">
+                <Graph />                
+            </div>
             <div id="detail">
                 <Outlet />
             </div>
