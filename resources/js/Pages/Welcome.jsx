51891580bdf99a2bf89b5b519c603057a4fa66cb
Linus Nagel <linusnag@gmail.com>
2025-01-02 21:09:22 +0100

    Update Welcome.jsx

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -351,7 +351,6 @@ export default function Welcome({ auth }) {
                         </main>
 
                         <footer className="py-16 text-center text-sm text-black dark:text-white/70">
-                            Laravel v{laravelVersion} (PHP v{phpVersion})
                         </footer>
                     </div>
                 </div>
