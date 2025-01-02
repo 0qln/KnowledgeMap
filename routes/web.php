548680f119186f206a708b8ec8228c6d6122e148
Linus Nagel <linusnag@gmail.com>
2025-01-02 21:14:28 +0100

    Add auth via entra id
    Note: there seems to be an issue with the tenant, which means the feature is right now unusable

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,14 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
+        'canRegister' => false,
     ]);
 });
 
@@ -20,5 +22,11 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('azure')->redirect();
+})->name('microsoft.login');
+
+Route::get('/auth/callback', [AuthenticatedSessionController::class, 'handleMicrosoftCallback']);
 
 require __DIR__.'/auth.php';
