bfa075c56a6bab3c5e4263e1a8f97b614b783298
Linus Nagel <linusnag@gmail.com>
2025-01-15 20:44:12 +0100

    fix node/show clipping

--- resources/js/Pages/Node/Show.jsx
+++ resources/js/Pages/Node/Show.jsx
@@ -2,7 +2,6 @@ import { Head, Link } from '@inertiajs/react';
 import GraphDetailsLayout from '@/Layouts/GraphDetailsLayout';
 import GraphLayout from '@/Layouts/GraphLayout';
 import { useGraph } from '@/Hooks/useGraph';
-import { nodeEq } from '@/Components/Graph';
 
 function Node({ node }) {
     const { setNodes, resetLinksForNode } = useGraph();
@@ -48,7 +47,7 @@ function Node({ node }) {
                                     onSuccess={onRestore}
                                     className="rounded-md bg-slate-600 w-6 h-6 mr-2 transition duration-150 ease-in-out hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 p-0.5">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"  className="stroke-white"/>
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" className="stroke-white" />
                                     </svg>
                                 </Link>
                             ) : (
@@ -72,7 +71,7 @@ function Node({ node }) {
                         [#{node.id}] {node.full_name}
                     </div>
                     <br />
-                    <div className="dark:text-gray-200 text-sm break-words w-full">
+                    <div className="dark:text-gray-200 text-sm break-words w-full max-h-80 overflow-auto">
                         {node.description}
                     </div>
                     <br />
@@ -81,18 +80,22 @@ function Node({ node }) {
                         <div>Tags</div>
                         <div className="flex-grow border-t-[1px] mx-2 dark:border-gray-400" />
                     </div>
-                    {node.tags && (
-                        <div className="flex flex-wrap text-sm break-words">
-                            {node.tags.map(tag => (
-                                <Link
-                                    href={route('dashboard.tags.show', tag.id)}
-                                    key={tag.id}
-                                    className="items-center m-1 text-white bg-gray-700 py-1 px-2 rounded-md transition duration-150 ease-in-out hover:bg-gray-600 flex flex-row space-x-1">
-                                    {tag.name}
-                                </Link>
-                            ))}
+                    <div>
+                        <div className="overflow-auto max-h-60">
+                            {node.tags && (
+                                <div className="flex flex-wrap text-sm break-words">
+                                    {node.tags.map(tag => (
+                                        <Link
+                                            href={route('dashboard.tags.show', tag.id)}
+                                            key={tag.id}
+                                            className="items-center m-1 text-white bg-gray-700 py-1 px-2 rounded-md transition duration-150 ease-in-out hover:bg-gray-600 flex flex-row space-x-1">
+                                            {tag.name}
+                                        </Link>
+                                    ))}
+                                </div>
+                            )}
                         </div>
-                    )}
+                    </div>
                 </div>
             </div>
         </div>
