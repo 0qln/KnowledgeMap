c727bf11dc553ed37f6c5a9d75222c43484bd059
Linus Nagel <linusnag@gmail.com>
2025-01-15 20:23:42 +0100

    use hamburger menu

--- resources/js/Layouts/GraphDetailsLayout.jsx
+++ resources/js/Layouts/GraphDetailsLayout.jsx
@@ -1,6 +1,6 @@
-export default function GraphDetailsLayout({ children }) {
+export default function GraphDetailsLayout({ children, overflow = false }) {
     return (
-        <div className="max-w-[30vw] flex-grow dark:bg-gray-800 m-4 p-4 rounded-md shadow-2xl max-h-[80vh] block overflow-hidden">
+        <div className={`max-w-[30vw] flex-grow dark:bg-gray-800 m-4 p-4 rounded-md shadow-2xl max-h-[80vh] block ${overflow ? 'overflow-show' : 'overflow-hidden'}`}>
             {children}
         </div>
     );
