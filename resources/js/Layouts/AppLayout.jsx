b6368bf40cfdc3d5238421f9c6021fec171c94c5
Linus Nagel <linusnag@gmail.com>
2025-01-15 20:54:34 +0100

    fix hamburger activation button

--- resources/js/Layouts/AppLayout.jsx
+++ resources/js/Layouts/AppLayout.jsx
@@ -57,7 +57,7 @@ export default function AppLayout({ childrenRight }) {
                             <div className="flex flex-col">
                                 <Dropdown>
                                     <Dropdown.Trigger>
-                                        <button type="button">
+                                        <button type="button" className="items-center flex">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" className="stroke-white"/>
                                             </svg>
