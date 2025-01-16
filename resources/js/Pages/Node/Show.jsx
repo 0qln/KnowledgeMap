8639aa7ec1b9a172d0b4b84d06a2aef146675cdd
Linus Nagel <linusnag@gmail.com>
2025-01-16 03:02:14 +0100

    Update Show.jsx

--- resources/js/Pages/Node/Show.jsx
+++ resources/js/Pages/Node/Show.jsx
@@ -3,9 +3,10 @@ import GraphDetailsLayout from '@/Layouts/GraphDetailsLayout';
 import GraphLayout from '@/Layouts/GraphLayout';
 import { useGraph } from '@/Hooks/useGraph';
 import { nodeEq } from '@/Components/Graph';
+import { Button } from '@headlessui/react';
 
 function Node({ node }) {
-    const { setNodes, resetLinksForNode } = useGraph();
+    const { setNodes, resetLinksForNode, updateDisplayRule } = useGraph();
     const onDelete = () => {
         setNodes(prevNodes => prevNodes.map(n => n.id === node.id ? { ...n, deleted: true } : n));
         resetLinksForNode(node);
@@ -14,6 +15,9 @@ function Node({ node }) {
         setNodes(prevNodes => prevNodes.map(n => n.id === node.id ? { ...n, deleted: false } : n));
         resetLinksForNode(node);
     };
+    const highlightNode = node => {
+        updateDisplayRule("highlightNodes", [node.id]); 
+    }
 
     return (
         <div>
@@ -48,7 +52,7 @@ function Node({ node }) {
                                     onSuccess={onRestore}
                                     className="rounded-md bg-slate-600 w-6 h-6 mr-2 transition duration-150 ease-in-out hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 p-0.5">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"  className="stroke-white"/>
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" className="stroke-white" />
                                     </svg>
                                 </Link>
                             ) : (
@@ -63,6 +67,14 @@ function Node({ node }) {
                                     </svg>
                                 </Link>
                             )}
+                            <Button
+                                onClick={() => highlightNode(node)}
+                                title="Highlight"
+                                className="rounded-md bg-slate-600 w-6 h-6 mr-2 transition duration-150 ease-in-out hover:bg-slate-500 focus:outline-none focus:ring-2 focus:ring-offset-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 p-0">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59" className="stroke-white" />
+                                </svg>
+                            </Button>
                         </div>
                         <div className={`dark:text-white text-xl inline break-words ${node.is_deleted ? "line-through" : ""}`}>
                             {node.title}
